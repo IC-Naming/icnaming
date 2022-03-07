ccce2cb81f48907f00b23f3c99708a45e857a9b8
Roy <royandroll@outlook.com>
2022-03-07 15:59:49 +0800

    1

--- src/sh_setup_dev.sh
+++ src/sh_setup_dev.sh
@@ -1,7 +1,6 @@
 npm install
 npm i typescript -g
 npm i ts-node -g
-npm i tsconfig-paths -g
 npm i jake -g
 npm i @cucumber/cucumber -g
 ts-node -r tsconfig-paths/register scripts/create_identities.ts
