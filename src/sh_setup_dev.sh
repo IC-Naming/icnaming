b76316a849e04a91a790a756feda4fdd20c4268d
icnaming-roy.icp <royandroll@outlook.com>
2022-06-13 15:34:37 +0800

    add scope

--- src/sh_setup_dev.sh
+++ src/sh_setup_dev.sh
@@ -1,4 +1,5 @@
 npm config set @deland-labs:registry https://www.myget.org/F/ic-feed/npm/
+npm config set @icnaming:registry https://www.myget.org/F/icnaming/npm/
 npm install
 npx icdev init-identity
 npx ts-node -r tsconfig-paths/register scripts/index.ts update-local-config
