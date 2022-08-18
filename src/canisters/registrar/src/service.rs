a7533b0ee8c1fa63f50d376cb817e64a07aecd9e
vanilla <104433129+extabr@users.noreply.github.com>
2022-08-18 18:51:40 +0800

    fmt

--- src/canisters/registrar/src/service.rs
+++ src/canisters/registrar/src/service.rs
@@ -28,7 +28,7 @@ use common::permissions::{
     must_be_in_named_canister, must_be_named_canister, must_be_system_owner,
 };
 use common::permissions::{must_be_named_principal, must_not_anonymous};
-use common::token_identifier::{get_valid_canister_id, get_valid_token_index, TokenIdentifier};
+use common::token_identifier::{get_valid_token_index, TokenIdentifier};
 use common::{AuthPrincipal, CallContext, TimeInNs};
 
 use crate::name_locker::{try_lock_name, unlock_name};
