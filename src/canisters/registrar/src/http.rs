d761c03d502369d6870bd6b6fe614bada6990fd2
vanilla <104433129+extabr@users.noreply.github.com>
2022-08-24 18:33:25 +0800

    update

--- src/canisters/registrar/src/http.rs
+++ src/canisters/registrar/src/http.rs
@@ -61,3 +61,64 @@ fn http_request(req: HttpRequest) -> HttpResponse {
         },
     }
 }
+
+mod test_http_request {
+    use super::*;
+    use crate::state::STATE;
+    use crate::token_index_store::RegistrationName;
+    use common::named_canister_ids::{get_named_get_canister_id, CanisterNames};
+    use common::token_identifier::{encode_token_id, CanisterId, TokenIndex};
+    use rstest::*;
+    use test_common::create_test_name;
+    use test_common::user::mock_now;
+    use test_common::user::mock_user1;
+
+    fn registration_init(name: String, user: Principal, now: u64) {
+        STATE.with(|s| {
+            let mut store = s.token_index_store.borrow_mut();
+            store.try_add_registration_name(RegistrationName(name.to_string()));
+            let mut store = s.registration_store.borrow_mut();
+            store.add_registration(Registration::new(
+                user.clone(),
+                name.to_string(),
+                now + 1,
+                now,
+            ));
+        });
+    }
+
+    #[rstest]
+    fn test_ntf_routing(mock_user1: Principal, mock_now: u64) {
+        let test_icnaming_str = create_test_name("icnaming");
+        let test_hello_str = create_test_name("hello");
+        registration_init(test_icnaming_str.to_string(), mock_user1, mock_now);
+        registration_init(test_hello_str.to_string(), mock_user1, mock_now);
+        let canisterid = get_named_get_canister_id(CanisterNames::Registrar);
+        let icnaming_token_id = encode_token_id(CanisterId(canisterid), TokenIndex(1u32));
+        let hello_token_id = encode_token_id(CanisterId(canisterid), TokenIndex(2u32));
+
+        let icnaming_request = HttpRequest {
+            url: format!("/?tokenid={}", icnaming_token_id),
+            method: "GET".to_string(),
+            headers: vec![],
+            body: vec![],
+        };
+        let hello_request = HttpRequest {
+            url: format!("/?tokenid={}", hello_token_id),
+            method: "GET".to_string(),
+            headers: vec![],
+            body: vec![],
+        };
+        let response = http_request(icnaming_request);
+        assert_eq!(response.status_code, 200);
+        assert!(String::from_utf8(response.body.to_vec())
+            .unwrap()
+            .contains("icnaming"));
+
+        let response = http_request(hello_request);
+        assert_eq!(response.status_code, 200);
+        assert!(String::from_utf8(response.body.to_vec())
+            .unwrap()
+            .contains("hello"));
+    }
+}
