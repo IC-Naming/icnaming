13f72c08cd21fb7b8541b8a0a0d81fe0b13fc579
vanilla <104433129+extabr@users.noreply.github.com>
2022-08-26 13:26:28 +0800

    update

---
+++ src/scripts/features/registrar_nft_query.feature
@@ -0,0 +1,54 @@
+@registrar
+Feature: Yumi market query Api
+
+  Background:
+    Given Reinstall registrar related canisters
+    Then Admin import names as following
+      | name      | owner | years |
+      | name1.ic  | user1 | 1     |
+      | na2.ic    | user1 | 2     |
+      | iiiiii.ic | user2 | 3     |
+
+  Scenario: Metadata
+    Then registrar get_details "name1.ic" result is
+      | key        | value    |
+      | owner      | user1    |
+      | name       | name1.ic |
+      | expired_at | 1        |
+      | created_at | 0        |
+    Then registrar metadata "name1.ic" result is
+      | key  | value    |
+      | name | name1.ic |
+
+
+  Scenario: getTokens
+    Then registrar get_details "name1.ic" result is
+      | key        | value    |
+      | owner      | user1    |
+      | name       | name1.ic |
+      | expired_at | 1        |
+      | created_at | 0        |
+    Then registrar getTokens result is
+      | key  | value    |
+      | name | name1.ic |
+
+
+  Scenario: getRegistry
+    Then registrar get_details "name1.ic" result is
+      | key        | value    |
+      | owner      | user1    |
+      | name       | name1.ic |
+      | expired_at | 1        |
+      | created_at | 0        |
+    Then registrar getRegistry result is
+      | key  | value    |
+      | name | name1.ic |
+
+  Scenario: supply
+    Then registrar get_details "name1.ic" result is
+      | key        | value    |
+      | owner      | user1    |
+      | name       | name1.ic |
+      | expired_at | 1        |
+      | created_at | 0        |
+    Then registrar supply result is "3"
---
+++ src/scripts/features/step_definitions/registrar_nft.steps.ts
@@ -0,0 +1,44 @@
+import "./setup"
+import {DataTable, Given, Then, When} from '@cucumber/cucumber'
+import {createRegistrar, registrar} from '~/declarations/registrar'
+import {registrar_control_gateway} from '~/declarations/registrar_control_gateway'
+import {createDicp, dicp} from '~/declarations/dicp'
+import {assert, expect} from 'chai'
+import {reinstall_all} from '~/../tasks'
+import {
+    BooleanActorResponse as AvailableResult,
+    BooleanActorResponse as RegisterWithQuotaResult,
+    BooleanActorResponse as TransferResult,
+    BooleanActorResponse as TransferFromResult,
+    BooleanActorResponse as TransferByAdminResult,
+    BooleanActorResponse as RenewNameResult,
+    QuotaType,
+    GetDetailsActorResponse as RegisterWithPaymentResponse,
+    GetNameStatueActorResponse,
+} from '~/declarations/registrar/registrar.did'
+import {identities} from '~/identityHelper'
+import {Result} from '~/utils/Result'
+import {assert_remote_result} from './utils'
+import logger from 'node-color-log'
+import fs from 'fs'
+import {canister, utils} from '@deland-labs/ic-dev-kit'
+import {
+    AssignNameResponse,
+    ImportQuotaResponse
+} from '~/declarations/registrar_control_gateway/registrar_control_gateway.did'
+
+Then(/^registrar metadata "([^"]*)" result is$/, async function (name, table) {
+    const result = await registrar.metadata(name)
+    assert_remote_result(result, table)
+});
+Then(/^registrar getTokens result is$/, async function (name, table) {
+    const result = await registrar.getTokens()
+    assert_remote_result(result, table)
+});
+Then(/^registrar getRegistry result is$/, function () {
+
+});
+Then(/^registrar supply result is "([^"]*)"$/, async function (count) {
+    const result = await registrar.supply()
+    assert_remote_result(result, count)
+});
